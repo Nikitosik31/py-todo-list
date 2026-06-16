757008c85d195abb637a2bd32167500cd6aaa381
Nik <nikita.pitushkin31@mail.com>
2026-06-16 22:53:51 +0300

    feat: add Task and Tag models, admin, URLs and ListView for tasks

---
+++ todo/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    TaskListView,
+)
+
+
+app_name = "todo"
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    ordering = ["-created_at"]
+    context_object_name = "task_list"
+    paginate_by = 5
+
+
